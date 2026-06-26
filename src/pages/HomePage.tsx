5dd7d6210a554640dc70c5813fff71d3d385bc5d
Cristhian Castillo <kal2403199255@gmail.com>
2026-06-26 06:36:19 +0200

    feat: implement hero section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import { Hero } from "../components/sections/Hero";
+
 export const HomePage = () => {
     return (
-        <section className="flex min-h-[70vh] items-center justify-center">
-            <h1 className="text-5xl font-bold">Portfolio Layout Ready</h1>
-        </section>
-    );
-};
+        <>
+            <Hero />
+        </>
+
+    )
+}
